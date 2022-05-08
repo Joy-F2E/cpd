5d6dc368bf52da358a46cd251a4b555d72499951
Joy <799840822@qq.com>
2022-05-09 01:32:56 +0800

    feat # add vue-composables demo

---
+++ vue-composables/shims-vue.d.ts
@@ -0,0 +1,8 @@
+declare module '*.vue' {
+    // import { ComponentOptions } from 'vue'
+    // const componentOptions: ComponentOptions
+    // export default componentOptions
+    import Vue from 'vue';
+    const vue: Vue;
+    export default vue
+}
